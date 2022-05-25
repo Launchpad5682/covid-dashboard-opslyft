e46d8ca29d8ea64d4bcf408f362a504c495d7972
launchpad5682 <launchpad5682@pop-os.localdomain>
2022-05-26 02:28:02 +0530

    feat: Dashboard assembled

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChangeEvent, useState } from "react";
+import DashboardCount from "./components/DashboardCount";
+import { LineChart } from "./components/LineChart";
+import { Table } from "./components/Table";
+import { useDataProvider } from "./context/data-context";
 
 function App() {
+  const {
+    dashboardCount: { active, recovered, deceased },
+    timeSeriesData,
+    lastDays,
+    dispatch,
+  } = useDataProvider();
+
+  const [mode, setMode] = useState<"cases" | "recovered" | "deaths">("cases");
+
+  const daysChangeHandler = (event: ChangeEvent<HTMLSelectElement>) => {
+    const value = Number(event.currentTarget.value);
+
+    if (value !== 0) {
+      dispatch({ type: "SET_LAST_DAYS", payload: { lastDays: value } });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="h-screen w-full overflow-y-auto bg-0000-900 text-xl flex flex-col gap-5 items-center">
+      <span className="text-4xl text-black mt-5">COVID DASHBOARD</span>
+      <div className="flex justify-center gap-10">
+        <DashboardCount count={active} heading="active" color="red" />
+        <DashboardCount count={recovered} heading="recovered" color="green" />
+        <DashboardCount count={deceased} heading="deceased" color="black" />
+      </div>
+      <div className="flex flex-row gap-5 w-full max-w-7xl justify-between">
+        <span className="text-black">
+          Showing Stats of Last {lastDays} days
+        </span>
+        <select className="" onChange={daysChangeHandler}>
+          <option value="30">30 days</option>
+          <option value="60">60 days</option>
+          <option value="90">90 days</option>
+        </select>
+      </div>
+      <div className="w-full max-w-7xl">
+        {Object.keys(timeSeriesData).length > 0 && (
+          <LineChart data={timeSeriesData} mode={mode} />
+        )}
+      </div>
+      <div>
+        <button
+          className={`bg-gray-500 text-white p-4 rounded-sm ${
+            mode === "cases" && "bg-gray-600"
+          }`}
+          onClick={() => setMode("cases")}
         >
-          Learn React
-        </a>
-      </header>
+          Cases
+        </button>
+        <button
+          className={`bg-gray-500 text-white p-4 rounded-sm ${
+            mode === "recovered" && "bg-gray-600"
+          }`}
+          onClick={() => setMode("recovered")}
+        >
+          Recoverd
+        </button>
+        <button
+          className={`bg-gray-500 text-white p-4 rounded-sm ${
+            mode === "deaths" && "bg-gray-600"
+          }`}
+          onClick={() => setMode("deaths")}
+        >
+          Deceased
+        </button>
+      </div>
+      <Table />
     </div>
   );
 }
